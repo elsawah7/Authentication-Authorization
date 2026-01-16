0f9fdfa1fb9191ca74f307f4835664a04e284aa8
Zezo Elsawah <zezoelsawah7@gamil.com>
2026-01-16 20:10:41 +0200

    Add Feature Authentication with Google

---
+++ 5_Auth_with_Google/app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    public function callback(Request $request)
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::firstOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'password' => Hash::make(uniqid()), // Generate a random password
+                'email_verified_at' => now(),
+            ]
+        );
+        Auth::login($user);
+        return redirect()->intended('profile');
+    }
+}
---
+++ 5_Auth_with_Google/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VerifyAcountMail;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return back()->with('error', 'Invalid credentials provided.');
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return back()->with('error', 'Invalid credentials provided.');
+        }
+
+        if (!$user->email_verified_at) {
+            Mail::to($user->email)->send(new VerifyAcountMail($user->otp, $user->email));
+            return redirect()->route('email.verify', ['email' => $user->email])->with('info', 'Please verify your email before logging in. An OTP has been sent to your email address.');
+        }
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return redirect()->intended('profile');
+        }
+        return back()->with('error', 'Invalid credentials provided.');
+    }
+}
