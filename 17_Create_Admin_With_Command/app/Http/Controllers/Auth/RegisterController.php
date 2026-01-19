3476e3b4387e3bf08c5ecda7dd0501aa4109b6e1
Zezo Elsawah <zezoelsawah7@gamil.com>
2026-01-19 18:37:41 +0200

    Add Feature Create Admin With Command

---
+++ 17_Create_Admin_With_Command/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Mail\VerifyAcountMail;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        $otp = rand(100000, 999999);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => $request->role,
+            'password' => Hash::make($request->password),
+            'otp' => $otp,
+        ]);
+
+        Mail::to($user->email)->send(new VerifyAcountMail($otp, $user->email));
+        return redirect()->route('account.verify', $user->email);
+    }
+}
